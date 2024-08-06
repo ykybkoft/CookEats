859d2d14e337c4d50843ee223f7e9d5589f64c01
joojoo <987654hi@naver.com>
2024-08-06 10:04:59 +0900

    BoardNormal, BoardNormalComment table 수정

--- src/main/java/com/project/cookEats/board_normal/BoardNormalComment.java
+++ src/main/java/com/project/cookEats/board_normal/BoardNormalComment.java
@@ -1,11 +1,13 @@
 package com.project.cookEats.board_normal;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
+import com.project.cookEats.member.Member;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 import org.springframework.data.annotation.Id;
 
 import java.sql.Date;
@@ -15,12 +17,21 @@ import java.sql.Date;
 @Entity
 @ToString
 public class BoardNormalComment {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String board_normal;
-    private String member;
+
+    @ManyToOne(cascade = CascadeType.MERGE)
+    @JoinColumn()
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Member member;
+
+    @Column(name = "sysDate", nullable = false, updatable = false,  columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
     private Date sys_date;
+
+    @Column(length = 255, nullable = false)
     private String comment_contents;
-    private Integer comment_like;
+
+    @Column(name = "cmtLike")
+    @ColumnDefault("0")
+    private long comment_like;
 }
