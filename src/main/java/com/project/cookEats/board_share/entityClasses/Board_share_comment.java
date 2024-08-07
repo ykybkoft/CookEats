4291d902b6df6d86e686c3d601cf9f91d1e0d693
CatSayjjaek <jeonmyungse@naver.com>
2024-08-07 15:34:38 +0900

    Re_board_share_request (#14)
    * merge member, board_recipe DB table
    
    * Modify Member table (#2)
    
    * 기본틀 잡기 & 회원가입 프로세스 생성 (#3)
    
    * Modify Member table
    
    * 기본틀 잡기 & Test 용 회원가입 로직 생성
    
    * Myeongse (#4)
    
    * create_board_share
    
    * Modify_shareTable
    
    * Modify_shareTable08-05
    
    * Spring Security 기본 설정 & 로그인 기능 구현 (#5)
    
    * homeHeader_and_css
    
    * homeHeader_and_css (#6)
    
    * Modify_header_home_css
    
    * Modity_board_share
    
    ---------
    
    Co-authored-by: jihun <maru.workstation@gmail.com>
    Co-authored-by: ykybkoft <126169447+ykybkoft@users.noreply.github.com>

--- src/main/java/com/project/cookEats/board_share/entityClasses/Board_share_comment.java
+++ src/main/java/com/project/cookEats/board_share/entityClasses/Board_share_comment.java
@@ -1,4 +1,4 @@
-package com.project.cookEats.board_share;
+package com.project.cookEats.board_share.entityClasses;
 
 import com.project.cookEats.member.Member;
 import jakarta.persistence.*;
@@ -8,12 +8,9 @@ import lombok.ToString;
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.sql.Date;
-import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -25,21 +22,26 @@ public class Board_share_comment {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
-    private String username;
-
-    @ManyToOne(cascade = CascadeType.MERGE)
-    @JoinColumn()
+    // 댓글 작성자
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
     @OnDelete(action = OnDeleteAction.CASCADE)
     private Member member;
-
-    @Column(name = "sysDate", nullable = false, updatable = false,  columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
-    private Date sysDate;
 
     @Column(length = 255, nullable = false)
     private String comment;
 
+    @Column(name = "sysDate", nullable = false, updatable = false,  columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    private Date sysDate;
+
     @Column(name = "cmtLike")
     @ColumnDefault("0")
     private int cmtLike;
+
+    // 탈퇴시 정보 삭제를 위한 컬럼
+    // 코멘트는 많은 사용자가 가질 수 있다. 즉, n:1 양방향 관계 설정
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_share_id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Board_share board_share;
 }
