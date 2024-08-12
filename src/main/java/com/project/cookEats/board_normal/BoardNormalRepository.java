dbab9c850b9cac1f571bef464bd14121743bfbfe
joojoo <987654hi@naver.com>
2024-08-12 09:26:13 +0900

    병합 변경

--- src/main/java/com/project/cookEats/board_normal/BoardNormalRepository.java
+++ src/main/java/com/project/cookEats/board_normal/BoardNormalRepository.java
@@ -1,19 +1,15 @@
 package com.project.cookEats.board_normal;
 
-import com.project.cookEats.member.Member;
-import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.awt.print.Pageable;
-import java.util.List;
 import java.util.Optional;
 
-public interface BoardNormalRepository extends JpaRepository<Member, Long> {
+public interface BoardNormalRepository extends JpaRepository<BoardNormal, Long> {
 
 
-    Optional<Member> findByUsername(String username);
-
-//    Page<BoardNormal> findPageBy(Pageable page);
-//    List<BoardNormal> findAllByTitleContains(String title);
+//    Optional<BoardNormal> findById(String id);
+//    Optional<BoardNormal> findByTitle(String title);
+//    Optional<BoardNormal> findByMember(String member);
+//    Optional<BoardNormal> findByContent(String content);
 
 }
