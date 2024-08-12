1e68267417066ef052abf9ed03f9c2eafcecc3e7
joojoo <987654hi@naver.com>
2024-08-12 15:28:08 +0900

    게시판 데이터 불러오기

---
+++ src/main/java/com/project/cookEats/board_normal/BoardNormal.java
@@ -0,0 +1,53 @@
+package com.project.cookEats.board_normal;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.project.cookEats.member.Member;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@ToString
+public class BoardNormal {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 100, nullable = false)
+    private String title;
+
+    @ManyToOne(cascade = CascadeType.MERGE)
+    @JoinColumn()
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Member member;
+
+    @Column(name = "sysDate", nullable = false, updatable = false,  columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    @CreatedDate
+    private Date sys_date;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    @Column(name = "count")
+    @ColumnDefault("0")
+    private int count;
+
+    @Column(name = "llike")
+    @ColumnDefault("0")
+    private int llike;
+
+    @JsonManagedReference
+    @OneToMany()
+    private List<BoardNormalComment> commentList = new ArrayList<>();
+
+}
---
+++ src/main/java/com/project/cookEats/board_normal/BoardNormalComment.java
@@ -0,0 +1,38 @@
+package com.project.cookEats.board_normal;
+
+import com.project.cookEats.member.Member;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@Entity
+@ToString
+public class BoardNormalComment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(cascade = CascadeType.MERGE)
+    @JoinColumn()
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Member member;
+
+    @Column(name = "sysDate", nullable = false, updatable = false,  columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    private Date sys_date;
+
+    @Column(length = 255, nullable = false)
+    private String comment_contents;
+
+    @Column(name = "cmtLike")
+    @ColumnDefault("0")
+    private long comment_like;
+
+}
