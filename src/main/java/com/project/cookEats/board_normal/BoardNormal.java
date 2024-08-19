671d96205d8f03919f7270cf6f02aa5ff18a0a07
Myeongse Jeon <jeonmyungse@naver.com>
2024-08-19 15:00:35 +0900

    병합 충돌 파일 정리 (#35)
    * homeHeader_and_css
    
    * Modify_header_home_css
    
    * Modity_board_share
    
    * Pile_file_remove
    
    * Modify_Re
    
    * Modify_re
    
    * Hyeonjoo (#25)
    
    * BoardNormal, BoardNormalComment table create
    
    * merge member, board_recipe DB table
    
    * Modify Member table (#2)
    
    * 기본틀 잡기 & 회원가입 프로세스 생성 (#3)
    
    * Modify Member table
    
    * 기본틀 잡기 & Test 용 회원가입 로직 생성
    
    * Myeongse (#4)
    
    * create_board_share
    
    * Modify_shareTable
    
    * Modify_shareTable08-05
    
    * Spring Security 기본 설정 & 로그인 기능 구현 (#5)
    
    * homeHeader_and_css (#6)
    
    * Modify_HeaderAndHome_css (#7)
    
    * homeHeader_and_css
    
    * Modify_header_home_css
    
    * BoardNormal, BoardNormalComment table create 2
    
    * BoardNormal, BoardNormalComment table 수정
    
    * Hyeonjoo (#8)
    
    * BoardNormal, BoardNormalComment table create
    
    * BoardNormal, BoardNormalComment table create 2
    
    * BoardNormal, BoardNormalComment table 수정
    
    * board.html 수정
    
    * 게시판 기능 구현
    
    * aa
    
    * html.css 수정
    
    * 검색창, 테이블 html.css 수정
    
    * 병합 변경
    
    * 게시판 데이터 불러오기
    
    * 게시판 데이터 불러오기
    
    ---------
    
    Co-authored-by: jihun <maru.workstation@gmail.com>
    Co-authored-by: ykybkoft <126169447+ykybkoft@users.noreply.github.com>
    Co-authored-by: CatSayjjaek <jeonmyungse@naver.com>
    
    * BoardShare_Create_Contents
    
    * BoardShare_Modify
    
    * Myeongse (#26)
    
    * merge member, board_recipe DB table
    
    * Modify Member table (#2)
    
    * 기본틀 잡기 & 회원가입 프로세스 생성 (#3)
    
    * Modify Member table
    
    * 기본틀 잡기 & Test 용 회원가입 로직 생성
    
    * Myeongse (#4)
    
    * create_board_share
    
    * Modify_shareTable
    
    * Modify_shareTable08-05
    
    * Spring Security 기본 설정 & 로그인 기능 구현 (#5)
    
    * homeHeader_and_css
    
    * homeHeader_and_css (#6)
    
    * Modify_header_home_css
    
    * Modity_board_share
    
    * Pile_file_remove
    
    * Modify_Re
    
    * Modify_re
    
    * BoardShare_Create_Contents
    
    * BoardShare_Modify
    
    ---------
    
    Co-authored-by: jihun <maru.workstation@gmail.com>
    Co-authored-by: ykybkoft <126169447+ykybkoft@users.noreply.github.com>
    
    * Hyeonjoo (#27)
    
    * BoardNormal, BoardNormalComment table create
    
    * merge member, board_recipe DB table
    
    * Modify Member table (#2)
    
    * 기본틀 잡기 & 회원가입 프로세스 생성 (#3)
    
    * Modify Member table
    
    * 기본틀 잡기 & Test 용 회원가입 로직 생성
    
    * Myeongse (#4)
    
    * create_board_share
    
    * Modify_shareTable
    
    * Modify_shareTable08-05
    
    * Spring Security 기본 설정 & 로그인 기능 구현 (#5)
    
    * homeHeader_and_css (#6)
    
    * Modify_HeaderAndHome_css (#7)
    
    * homeHeader_and_css
    
    * Modify_header_home_css
    
    * BoardNormal, BoardNormalComment table create 2
    
    * BoardNormal, BoardNormalComment table 수정
    
    * Hyeonjoo (#8)
    
    * BoardNormal, BoardNormalComment table create
    
    * BoardNormal, BoardNormalComment table create 2
    
    * BoardNormal, BoardNormalComment table 수정
    
    * board.html 수정
    
    * 게시판 기능 구현
    
    * aa
    
    * html.css 수정
    
    * 검색창, 테이블 html.css 수정
    
    * 병합 변경
    
    * 게시판 데이터 불러오기
    
    * 게시판 데이터 불러오기
    
    * 검색, 글쓰기 완료
    
    * 검색, 글쓰기 완료2
    
    ---------
    
    Co-authored-by: jihun <maru.workstation@gmail.com>
    Co-authored-by: ykybkoft <126169447+ykybkoft@users.noreply.github.com>
    Co-authored-by: CatSayjjaek <jeonmyungse@naver.com>
    
    * BoardShare_list_Create
    
    * BoardShare_detailPage
    
    * BoardShare_detailPage
    
    ---------
    
    Co-authored-by: hhJooJoo <160625211+hhJooJoo@users.noreply.github.com>
    Co-authored-by: jihun <maru.workstation@gmail.com>
    Co-authored-by: ykybkoft <126169447+ykybkoft@users.noreply.github.com>

--- src/main/java/com/project/cookEats/board_normal/BoardNormal.java
+++ src/main/java/com/project/cookEats/board_normal/BoardNormal.java
@@ -1,7 +1,6 @@
-
-
 package com.project.cookEats.board_normal;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.project.cookEats.member.Member;
 import jakarta.persistence.*;
@@ -13,6 +12,7 @@ import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 import org.springframework.data.annotation.CreatedDate;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -28,14 +28,15 @@ public class BoardNormal {
     @Column(length = 100, nullable = false)
     private String title;
 
+    @JsonBackReference(value = "boardNormal-member")
     @ManyToOne(cascade = CascadeType.MERGE)
     @JoinColumn()
     @OnDelete(action = OnDeleteAction.CASCADE)
     private Member member;
 
-    @Column(name = "sysDate", nullable = false, updatable = false,  columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    @Column(name = "sysDate", updatable = false,  columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
     @CreatedDate
-    private Date sys_date;
+    private LocalDateTime sys_date = LocalDateTime.now(); // 기본값 설정
 
     @Column(columnDefinition = "TEXT", nullable = false)
     private String content;
@@ -48,9 +49,10 @@ public class BoardNormal {
     @ColumnDefault("0")
     private int llike;
 
-    @JsonManagedReference
-    @OneToMany()
+    @JsonManagedReference(value = "boardNormal-comments")
+    @OneToMany(mappedBy = "boardNormal", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<BoardNormalComment> commentList = new ArrayList<>();
 
-}
+    private String formattedSysDate; // 포맷된 날짜를 저장할 필드
 
+}
