b04ca937aa0c296e8d98f814b00ab0ec3fb47f45
ykybkoft <126169447+ykybkoft@users.noreply.github.com>
2024-08-20 15:41:00 +0900

    home 좀 더 구성 (#38)

--- src/main/java/com/project/cookEats/HomeController.java
+++ src/main/java/com/project/cookEats/HomeController.java
@@ -2,6 +2,7 @@ package com.project.cookEats;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class HomeController {
@@ -9,4 +10,13 @@ public class HomeController {
     String home(){
         return "home.html";
     }
+
+    @GetMapping("/search")
+    String search(@PathVariable String search){
+        System.out.println(search);
+        return "search.html";
+    }
+
+
+
 }
--- src/main/java/com/project/cookEats/HomeService.java
+++ src/main/java/com/project/cookEats/HomeService.java
@@ -1,7 +1,5 @@
 package com.project.cookEats;
 
-import com.project.cookEats.board_recipe.RecipeDB;
-import com.project.cookEats.board_recipe.RecipeDBRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,8 +9,6 @@ import java.util.List;
 @Service
 public class HomeService {
 
-    private final RecipeDBRepository rdbr;
-    public List<RecipeDB> bestRecipe() {
 
         return rdbr.findTop5ByOrderByLLIKEDesc();
     }
