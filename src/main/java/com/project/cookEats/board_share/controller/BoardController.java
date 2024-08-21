d22fc6d537415b2bb98db7b952120be5424fcbd0
CatSayjjaek <jeonmyungse@naver.com>
2024-08-21 16:23:04 +0900

    crud_완성&&entity_modify

--- src/main/java/com/project/cookEats/board_share/controller/BoardController.java
+++ src/main/java/com/project/cookEats/board_share/controller/BoardController.java
@@ -16,19 +16,19 @@ import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/board_share")
+@RequestMapping("/boardShare")
 public class BoardController {
     // 의존성 주입
     private final Board_shareRepository br;
     private final Borad_shareService bs;
 
     // 게시글 리스트 페이지
-    @GetMapping("/list")
+    @GetMapping("/home")
     String title(Model model){
         List<Board_share> result = br.findAll();
-        model.addAttribute("list", result);
+        model.addAttribute("home", result);
 
-        return "board_share/list.html";
+        return "boardShare/home.html";
     }
 
     // 게시판 글 내용 보기 페이지 매핑
@@ -36,32 +36,32 @@ public class BoardController {
     String detail(@PathVariable Long id, Model model){
         model.addAttribute("data", bs.getContents(id));
 
-        return "board_share/view.html";
+        return "boardShare/view.html";
     }
 
     // 게시판 글 작성 페이지 매핑
-    @GetMapping("/b_write")
+    @GetMapping("/write")
     String write(Authentication auth, Model model){
         //게시글 로그인 된 유저의 아이디 가져옴 -my page- 활용
         Member result = bs.findMember(auth);
         model.addAttribute("nick",result.getNick());
 
-        return "board_share/b_write.html";
+        return "boardShare/write.html";
     }
 
-    @PostMapping("/b_write")
+    @PostMapping("/write")
     public String addPost(@ModelAttribute Board_share data) {
 
         bs.savePost(data);
 
-        return "redirect:/board_share/list";
+        return "redirect:/boardShare/home";
     }
     // 게시글 수정 데이터 불러오기
     @GetMapping("/edit/{id}")
     String edit(@PathVariable Long id, Model model){
         model.addAttribute("data", bs.getEdit(id));
 
-        return "board_share/edit.html";
+        return "boardShare/edit.html";
     }
     //게시글 수정
     @PostMapping("/edit")
@@ -70,7 +70,7 @@ public class BoardController {
         bs.editPost(data);
 
         // 리턴 뒤에 게시글 id를 붙여줘야 해당 수정된 개시글로 이동 됨.
-        return "redirect:/board_share/view/" + data.getId();
+        return "redirect:/boardShare/view/" + data.getId();
     }
 
     // 게시글 삭제
@@ -78,6 +78,6 @@ public class BoardController {
     public String delete(@PathVariable Long id){
 
         bs.deleteContents(id);
-        return "redirect:/board_share/list";
+        return "redirect:/boardShare/home";
     }
 }
