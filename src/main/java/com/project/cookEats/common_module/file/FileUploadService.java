19d913f4f3f04cc5a2b99614465d4b1298b0ea2c
jihun <maru.workstation@gmail.com>
2024-08-27 09:40:58 +0900

    08.27 commit

--- src/main/java/com/project/cookEats/common_module/file/FileUploadService.java
+++ src/main/java/com/project/cookEats/common_module/file/FileUploadService.java
@@ -4,24 +4,34 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.UUID;
 
 @Service
-public class FileUploadService {
+public class FileUpLoadService {
 
     @Value("${file.upload-dir}")
     private String uploadDir;
 
     public String saveFile(MultipartFile file) throws IOException {
-        String originalFileName = file.getOriginalFilename();
-        String uniqueFileName = UUID.randomUUID().toString() + "_" + originalFileName;
-        Path filePath = Paths.get(uploadDir, uniqueFileName);
-        Files.copy(file.getInputStream(), filePath);
+        if (file.isEmpty()) {
+            throw new IOException("Failed to store empty file.");
+        }
 
-        return uniqueFileName;
+        // Create the directory if it does not exist
+        File directory = new File(uploadDir);
+        if (!directory.exists()) {
+            directory.mkdirs();
+        }
+
+        // Resolve the file path
+        Path path = Paths.get(uploadDir).resolve(file.getOriginalFilename());
+        Files.copy(file.getInputStream(), path);
+
+        // Return the filename or the path for future reference
+        return file.getOriginalFilename();
     }
 }
--- src/main/java/com/project/cookEats/open_api/OpenApiRepository.java
+++
@@ -1,7 +0,0 @@
-package com.project.cookEats.open_api;
-
-import com.project.cookEats.board_recipe.RecipeDB;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OpenApiRepository extends JpaRepository<RecipeDB, Long> {
-}
