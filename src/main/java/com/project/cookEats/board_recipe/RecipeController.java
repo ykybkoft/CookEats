0596c5052386edf21f837184f88f09e1fdcf8e52
ykybkoft <126169447+ykybkoft@users.noreply.github.com>
2024-08-29 14:42:41 +0900

    경로 또 수정

--- src/main/java/com/project/cookEats/board_recipe/RecipeController.java
+++ src/main/java/com/project/cookEats/board_recipe/RecipeController.java
@@ -61,11 +61,8 @@ public class RecipeController {
         model.addAttribute("endPage", endPage);
 
         //혜정 코드
+        model.addAttribute("search", search);
 
-        if(searchType != null){
-            model.addAttribute("searchType",searchType);
-            model.addAttribute("search", search);
-        }
 
         return "boardRecipe/home"; // home.html로 반환
 
@@ -116,7 +113,7 @@ public class RecipeController {
     String write(Authentication auth, Model model){
         Member result = memberService.findMember(auth);
         model.addAttribute("user",result);
-        return "boardRecipe/write.html";
+        return "boardrecipe/write.html";
     }
 
     //혜정코드
